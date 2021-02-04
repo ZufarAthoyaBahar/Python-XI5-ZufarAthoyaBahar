3dbc97c67b81a5402e48e49857686f2cd528ab14
ZufarAthoyaBahar <athoyabahar@gmail.com>
2021-02-04 12:49:20 +0700

    master

--- 1_7 Januari/1_PROGRAM PENJUMLAHAN 2 BUAH BILANGAN.py
+++ 1_7 Januari/1_PROGRAM PENJUMLAHAN 2 BUAH BILANGAN.py
@@ -7,5 +7,6 @@ bil2 = input('masukkan bilangan kedua: ')
 # Menjumlah bilangan
 jumlah = float(bil1) + float(bil2)
 
+
 # Menampilkan jumlah
 print('jumlah {0} + {1} adalah {2}'.format(bil1, bil2, jumlah))
