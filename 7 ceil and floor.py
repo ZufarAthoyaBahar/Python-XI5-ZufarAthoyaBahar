2dcce199274b203f5fa0c26ef41e1a6a5f0c02d4
Zufar Athoya Bahar <78136074+ZufarAthoyaBahar@users.noreply.github.com>
2021-02-11 12:50:44 +0700

    11 Februati

---
+++ 7 ceil and floor.py
@@ -0,0 +1,13 @@
+from math import ceil, floor, trunc
+
+x = 187.65
+y = 122.9
+
+print()
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+print()
---
+++ 8 Faktorial.py
@@ -0,0 +1,7 @@
+from math import factorial
+
+x = factorial(5)
+y = factorial(7)
+z = factorial(6)
+
+print("{0}, {1}, {2}".format(x, y, z))
